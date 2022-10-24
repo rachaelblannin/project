3314863ebee54be4448bc7f00f40bec7c2da6fd1
rachaelblannin <rachblannin@gmail.com>
2022-10-24 16:30:22 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const server = app.listen(1045,() =>{
+    console.log(`server started succesfully on port ${server.address().port}`);
+})
