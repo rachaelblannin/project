50ed36c30927bda9e1d2ede6761646f40d718514
rachaelblannin <rachblannin@gmail.com>
2022-10-25 11:52:55 +0100

    requests

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // needs to be before the routes
+
 const routes = require("./routes.js");
 
 app.use(routes);
 
 app.use(express.json());
-
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-
-
 
 const server = app.listen(1045,() =>{
     console.log(`server started succesfully on port ${server.address().port}`);
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const routes = require("express").Router();
 const { spongeModel } = require ("./db");
 
 routes.post("/createCharacter", (req, res) => spongeModel.create(req.body)
-    .then(res.status(201).send(req.body)).catch(err => next(err)));
+    .then(result => res.status(201).send(result)).catch(err => next(err)));
 
 routes.get("/getall", (req, res) => spongeModel.find({}).then(results => res.send(results)).catch(err => next(err)));
 
